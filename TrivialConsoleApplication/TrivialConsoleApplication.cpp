aed7c7a33982ad31646c2bd490d7f1e782debd36
CanarianPlay <canaryplay33@gmail.com>
2023-10-25 20:39:51 +0200

    Add POO to TrivialPlatanito proyect

--- TrivialConsoleApplication/TrivialConsoleApplication.cpp
+++ TrivialConsoleApplication/TrivialConsoleApplication.cpp
@@ -2,6 +2,16 @@
 Crea un programa que haga preguntas sobre un tema específico (por ejemplo, historia, geografía, cine, etc.) y permita al usuario ingresar sus respuestas.
 Luego, imprima una puntuación al final basada en cuántas respuestas correctas proporcionó el usuario.*/
 
+/*Upgrade(POO): Crearemos una clase Question para representar una pregunta con su respuesta y un clase Quiz para manejar todo el juego.
+	Posteriormente agregaremos herencia creando un clase derivada de Quiz para diferentes temas*/
+
+
+/*TODO para ustedes mis platanitos:
+
+  1. Completa las dos clases derivadas que faltan
+  2. Modifica el código para que sea posible jugar con estas nuevas clases creadas
+
+*/
 
 #include <iostream>
 #include <string>
@@ -11,243 +21,207 @@ Luego, imprima una puntuación al final basada en cuántas respuestas correctas 
 
 using namespace std;
 
-enum Color
+// Clase base que representa una pregunta con su respuesta
+class Question 
 {
-	AZUL = 1,
-	ROJO,
-	VERDE,
-	AMARILLO
+	private:
+
+		string question;
+		string answer;
+
+	public:
+
+		//Constructor
+		Question(const string& q, const string& a) : question(q), answer(a) {};
+
+		//Destructor
+		~Question() {};
+
+		//Obtencion de la pregunta (question) (método get)
+		const string& getQuestion() 
+		{
+			return question;
+		}
+
+		//Obtencion de la respuesta (answer) (método get)
+		const string& getAnswer()
+		{
+			return answer;
+		}
+
+		//check de la respuesta (answer)
+		bool checkAnswer(const string& userAnswer)
+		{
+			string lowerUserAnswer = userAnswer;
+
+			transform(lowerUserAnswer.begin(), lowerUserAnswer.end(), lowerUserAnswer.begin(), ::tolower);
+
+			return userAnswer == answer;
+		}
 };
 
 
-//Variable global
-char continuar;
 
-//Declaración: Funcion para mostrar el mensaje de selección de la carta
-void mostrarMensajeTematicaCarta(Color carta);
+//Clase base que representa el juego
+class Quiz
+{
+	protected:
+		int score;
+		vector<Question> questions;
 
-//Declaracion: Funcion para obtener la respuesta del usuario
-string getUserResponse();
-//Declaracon: Funcion para jugar la ronda de preguntas
-void PlayRound(Color carta, int& score);
+	public:
 
-//Declaracion: Funcion para validar si el jugador gano o no
-bool winValidator(Color carte, int score);
+		//Constructor
+		Quiz() : score(0) {};
 
-//Declaracion: Funcion para convertir el color de la carta
-void stringTOEnumConverter(Color& carta);
+		//Destructor
+		~Quiz() {};
 
-//Declaracion: Funcion 2.0 para validar la respuesta
-bool validarRespuesta(const map<string, string>& preguntaResp, const string& pregunta, const string& respuesta, int& score);
+		//Añadir question (Add)
+		void addQuestion(const Question& question)
+		{
+			questions.push_back(question);
+		}
 
-int main()
+		//Jugamos
+		void play()
+		{
+			for (size_t i = 0; i < 3; i++)
+			{
+				int randomIndex = rand() % questions.size();
+				Question& currentQuestion = questions[randomIndex];
+
+				cout << currentQuestion.getQuestion() << endl;
+				string userAnswer;
+				std::getline(std::cin, userAnswer);
+				transform(userAnswer.begin(), userAnswer.end(), userAnswer.begin(), ::tolower);
+
+				if(currentQuestion.checkAnswer(userAnswer))
+				{
+					cout << "Respuesta correcta." << endl;
+					score++;
+				}
+				else
+				{
+					cout << "Respuesta incorrecta. La respuesta correcta era: " << currentQuestion.getAnswer() << endl;
+				}
+
+				questions.erase(questions.begin() + randomIndex);
+			}
+
+			printGameResult();
+		}
+
+		
+		virtual void printGameResult() const = 0;
+};
+
+
+//Clase derivada para un tema especifico (Deporte) (herencia)
+class SportQuiz : public Quiz
+{
+	public:
+
+		//Constructor
+		SportQuiz() 
+		{
+			addQuestion(Question("Quien gano la ultima liga espanola de futbol? ", "real madrid"));
+			addQuestion(Question("Quien es el mejor jugador del mundo? ", "messi"));
+			addQuestion(Question("En que quipo de formula 1 esta actualmente fernando alonso ", "aston martin"));
+			addQuestion(Question("Quien gano el ultimo rolland garros ? ", "nadal"));
+			addQuestion(Question("El equipo con mejores jugadores en la NBA en 1993", "chicago bulls"));
+		};
+
+		//Destructor
+		~SportQuiz() {};
+
+
+		void printGameResult() const override 
+		{
+			if(score == 3)
+			{
+				cout << "Felicidades acabas de ganar el platanito azul" << endl;
+			}
+			else 
+			{
+				cout << "Mira ver si estudiamos un poquito mas platanito " << endl;
+			}
+		}
+};
+
+//Clase dereivdada para un tema especifico (Historia) (herencia)
+class HistoryQuiz : public Quiz
 {
 
-	//Empieza el juego
-	cout << "Bienvenido al juego del platanito, cuanto mas aciertes más puntos obtendras" << endl;
+	public:
+
+		//Constructor
+		HistoryQuiz()
+		{
+
+			addQuestion(Question("Quien gano la segunda guerra mundial?  ", "aliados"));
+			addQuestion(Question("Quien descubiro la penicilina? ", "fleming"));
+			addQuestion(Question("Que paso en 1936 en espana ? ", "guerra civil"));
+			addQuestion(Question("Quienes fueron los reyes que ayudaron al descubrimiento de america? ", "reyes catolicos"));
+			addQuestion(Question("Quien fue el dictador de italia ?", "mussolini"));
+		}
+
+		//Destructor
+		~HistoryQuiz() {};
+
+		void printGameResult() const override
+		{
+			if (score == 3)
+			{
+				cout << "Felicidades acabas de ganar el platanito rojo" << endl;
+			}
+			else
+			{
+				cout << "Mira ver si estudiamos un poquito mas platanito " << endl;
+			}
+		}
+
+};
+
+//Funcion principal
+int main()
+{
+	srand(time(nullptr));
+	char continuar;
 
 	do
 	{
-		//Variables locales
-		int score = 0;
-		Color carta;
+		//Empieza el juego
+		cout << "Bienvenido al juego del platanito, cuanto mas aciertes más puntos obtendras" << endl;
 
-		stringTOEnumConverter(carta);
+		cout << "Elige entre las siguientes cartas: azul, rojo, verde, amarillo" << endl;
+		string cartaStr;
+		cin >> cartaStr;
+		transform(cartaStr.begin(),cartaStr.end(), cartaStr.begin(), ::tolower);
+		std::cin.ignore();
 
-		PlayRound(carta, score);
+		Quiz* quiz = nullptr;
 
+		if (cartaStr == "azul")
+		{
+			cout << "El tema será DEPORTES!!!" << endl;
+			quiz = new SportQuiz();
+		}
+		else if (cartaStr == "rojo") 
+		{
+			cout << "El tema será HISTORIA!!!" << endl;
+			quiz = new HistoryQuiz();
+		}
+		else 
+		{
+			cout << "No es tan difícil elegir entre 4 cartas enumeradas del 1 al 4" << endl;
+		}
 
-		winValidator(carta, score);
-		cout << endl;
+		quiz->play();
+		delete quiz;
+
 		cout << "Desea continuar ? (S/N) " << endl;
 		std::cin >> continuar;
 
 	} while (continuar != 'N');
-}
-
-//Implementacion:  Funcion para mostrar el mensaje de selección de la carta
-void mostrarMensajeTematicaCarta(Color carta)
-{
-	switch (carta)
-	{
-	case AZUL:
-		cout << "El tema a tratar es deportes" << endl;
-		break;
-	case ROJO:
-		cout << "El tema a tratar es historia" << endl;
-		break;
-	case VERDE:
-		cout << "El tema a tratar es cine" << endl;
-		break;
-	case AMARILLO:
-		cout << "El tema a tratar es política" << endl;
-		break;
-	default:
-		cout << "No es tan difícil elegir entre 4 cartas enumeradas del 1 al 4" << endl;
-		break;
-	}
-
-}
-
-//Implementacion: Funcion para obtener la respuesta del usuario
-string getUserResponse()
-{
-	//Variable local
-	string response = "";
-	std::getline(std::cin, response);
-	transform(response.begin(), response.end(), response.begin(), ::tolower);
-	return response;
-
-}
-
-//Implementacion: Funcion 2.0 para validar la respuesta
-bool validarRespuesta(const map<string, string>& preguntaResp, const string& pregunta, const string& respuesta, int& score)
-{
-	auto it = preguntaResp.find(pregunta);
-
-	if (it != preguntaResp.end())
-	{
-		if (respuesta == it->second)
-		{
-			score++;
-			return true;
-		}
-	}
-	return false;
-}
-
-//Implementacion: Funcion para jugar la ronda de preguntas
-void PlayRound(Color carta, int& score)
-{
-	map<string, string> preguntaRespuesta;
-
-	switch (carta)
-	{
-	case AZUL:
-		mostrarMensajeTematicaCarta(carta);
-
-		preguntaRespuesta = { 
-			{"Quien gano la ultima liga espanola de futbol? ", "real madrid"},
-			{"Quien es el mejor jugador del mundo? ", "messi" },
-			{"En que quipo de formula 1 esta actualmente fernando alonso ", "aston martin"},
-			{"Quien gano el ultimo rolland garros ? ", "nadal"},
-			{"El equipo con mejores jugadores en la NBA en 1993", "chicago bulls"}
-		};
-		break;
-	case ROJO:
-		mostrarMensajeTematicaCarta(carta);
-
-		preguntaRespuesta = {
-			{"Quien gano la segunda guerra mundial?  ", "aliados"},
-			{"Quien descubiro la penicilina? ", "fleming" },
-			{"Que paso en 1936 en espana ? ", "guerra civil"},
-			{"Quienes fueron los reyes que ayudaron al descubrimiento de america? ", "reyes catolicos"},
-			{"Quien fue el dictador de italia ?", "mussolini"}
-		};
-		break;
-	case VERDE:
-		mostrarMensajeTematicaCarta(carta);
-
-		preguntaRespuesta = {
-			{"Que actriz gano el ultimo oscar ?  ", "jessica chastain"},
-			{"Como se llama el mayor villano de todos los tiempo ? ", "darth vader" },
-			{"Que pelicula tiene mas oscars ? ", "titanic"},
-			{"Que superheroe de marvel es rojo y dorado ? ", "iron man"},
-			{"Como se llama el bxeador itloamericano más famoso del cine ?", "rocky balboa"}
-		};
-		break;
-	case AMARILLO:
-		mostrarMensajeTematicaCarta(carta);
-
-		preguntaRespuesta = {
-			{"Quien es actualmente el presidente de espana? ", "pedro sanchez"},
-			{"Cuantos poderes hay en espana? ", "3" },
-			{"Que partido es moradito ? ", "podemos"},
-			{"Que dos partidos hay en EEUU ? ", "democratas y republicanos"},
-			{"Que cargo tiene el gobernante de Japon ?", "primer ministro"}
-		};
-
-		break;
-	default:
-		cout << "No es tan dificil elegir entre 4 cartas enumeradas del 1 al 4 " << endl;
-		break;
-	}
-
-	for (int i = 0; i < 3; i++)
-	{
-		auto it = preguntaRespuesta.begin();
-		advance(it, rand() % preguntaRespuesta.size());
-		string pregunta = it->first;
-
-		cout << pregunta << endl;
-		string respuesta = getUserResponse();
-
-		validarRespuesta(preguntaRespuesta, pregunta, respuesta, score);
-
-		preguntaRespuesta.erase(pregunta);
-		
-	}
-}
-
-//Implementacion: Funcion para validar si el jugador gano o no
-bool winValidator(Color carta, int score)
-{
-	if (score == 3)
-	{
-		cout << "Felicidades acabas de ganar ... ";
-
-		switch (carta)
-		{
-		case AZUL:
-			cout << " el platanito azul " << endl;
-			break;
-		case ROJO:
-			cout << " el platanito rojo " << endl;
-			break;
-		case AMARILLO:
-			cout << " el platanito amarillo " << endl;
-			break;
-		case VERDE:
-			cout << " el platanito verde " << endl;
-			break;
-		default:
-			break;
-		}
-
-		return true;
-	}
-	else
-	{
-		cout << "Mira ver si estudiamos un poquito mas platanito " << endl;
-		return false;
-	}
-}
-
-//Implementacion: Funcion para convertir el color de la carta
-void stringTOEnumConverter(Color& carta)
-{
-	string cartaStr;
-	//Preguntar al usuario que tema quiere tratar
-	cout << "Elige entre las siguientes cartas: azul, rojo, verde, amarillo" << endl;
-	cin >> cartaStr;
-	std::cin.ignore();
-
-	transform(cartaStr.begin(), cartaStr.end(), cartaStr.begin(), ::tolower);
-
-	if (cartaStr == "azul")
-	{
-		carta = AZUL;
-	}
-	else if (cartaStr == "rojo")
-	{
-		carta = ROJO;
-	}
-	else if (cartaStr == "verde")
-	{
-		carta = VERDE;
-	}
-	else if (cartaStr == "amarillo")
-	{
-		carta = AMARILLO;
-	}
 }
